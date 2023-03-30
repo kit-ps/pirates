c076a5f0014ba5f96e695d1eb9d50a385a651664
Akim Stark <astark@kis-oker.fzi.de>
2023-03-30 15:34:24 +0200

    Initial commit

---
+++ client/src/client.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <chrono>
+#include <thread>
+#include <pthread.h>
+#include <openssl/aes.h>
+#include "seal/seal.h"
+
+int main(int argc, char **argv) {
+    std::cout << "Starting PIRATES client ..." << std::endl;
+    return 0;
+}
---
+++ master/src/master.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <openssl/aes.h>
+#include "seal/seal.h"
+
+int main(int argc, char **argv) {
+    std::cout << "Starting PIRATES master ..." << std::endl;
+    return 0;
+}
