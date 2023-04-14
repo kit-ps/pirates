6bd37b903be3d00903be227564c1e8a30421f115
Akim Stark <stark@fzi.de>
2023-04-14 10:49:42 +0200

    Added LPCNet Encoding and first measurements

--- master/src/master.cpp
+++ master/src/master.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,8 +11,39 @@
 #include <arpa/inet.h>
 #include <openssl/aes.h>
 #include "seal/seal.h"
+#include <boost/asio.hpp>
+
+std::string MASTER_IP = "";
+std::string WORKER_IP = "";
+int MESSAGE_SIZE;
+int NUM_MESSAGE;
+int NUM_ROUNDS;
+
+using boost::asio::ip::tcp;
 
 int main(int argc, char **argv) {
     std::cout << "Starting PIRATES master ..." << std::endl;
+    int c;
+    while ((c = getopt(argc, argv, "m:w:s:n:r:")) != -1) {
+        switch(c) {
+            case 'm':
+                MASTER_IP = std::string(optarg);
+                break;
+            case 'w':
+                WORKER_IP = std::string(optarg);
+                break;
+            case 'n':
+                NUM_MESSAGE = std::stoi(optarg);
+                break;
+            case 'r':
+                NUM_ROUNDS = std::stoi(optarg);
+                break;
+            default:
+                abort();
+        }
+    }
+
+    //////////////////////////////////////////// RECEIVING VOICE SNIPPETS /////////////////////////////////////
+    
     return 0;
 }
