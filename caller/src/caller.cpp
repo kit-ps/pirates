b5d330080719d0aa05d86e8fd85179019e5ef4a0
Christoph Coijanovic <hi@coijanovic.com>
2023-05-10 07:01:48 +0200

    no more files in caller

--- caller/src/caller.cpp
+++ caller/src/caller.cpp
@@ -60,68 +60,47 @@ int main(int argc, char **argv) {
     }
     
     std::cout << "Encoding voice snippet ..." << std::endl;
-    // Input and output files for input and encoded audio
-    FILE *fin_enc, *fout_enc, *fin_dec, *fout_dec;
+    // Open input file
+    FILE *input_file;
     std::string tmp = "/audio/raw_" + std::to_string(SNIPPET_SIZE) + "ms.wav";
-    fin_enc = fopen(tmp.c_str(), "rb");
-    if (fin_enc == NULL) {
+    input_file = fopen(tmp.c_str(), "rb");
+    if (!input_file) {
         std::cerr << "Failed to open raw audio file!" << std::endl;
         return 1;
     }
-    fout_enc = fopen("/home/app/pirates/raw_encoded", "wb");
+
+    std::vector<char> encoded_snippet;
     
     // direct_split = 0
     LPCNetFreeDV *lf = lpcnet_freedv_create(0);
     // Taken from lpcnet_enc.c
     char frame[lpcnet_bits_per_frame(lf)];
-    int f=0;
-    int bits_written=0;
     short pcm[lpcnet_samples_per_frame(lf)];
     // Read from wav file and encode each read frame
-    // To test, we only encode 1 frame
-    // How long is a LPCNet frame?
+    // Append frame to encoded_snippet vector
     auto start = std::chrono::high_resolution_clock::now();
     while (1) {      
-        int nread = fread(pcm, sizeof(short), lpcnet_samples_per_frame(lf), fin_enc);
+        int nread = fread(pcm, sizeof(short), lpcnet_samples_per_frame(lf), input_file);
         if (nread != lpcnet_samples_per_frame(lf)) break;
 
         lpcnet_enc(lf, pcm, frame);
-        bits_written += fwrite(frame, sizeof(char), lpcnet_bits_per_frame(lf), fout_enc);
-
-        fflush(stdin);
-        fflush(stdout);
-        f++;
+        for (char c : frame) {
+            encoded_snippet.push_back(c);
+        }
     }
     // Stop measuring time
     auto end = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
     std::cout << "Needed time for LPCNet encoding: " << duration.count() << "\u03bcs\n";
      // Close files for encoding
-    fclose(fin_enc);
-    fclose(fout_enc);
-    // Reopen encoded file
-    FILE* encoded_snippet = fopen("/home/app/pirates/raw_encoded", "rb");
-    if (!encoded_snippet) {
-        std::cerr << "Failed to open encoded voice snippet" << std::endl;
-        return 1;
-    }
-    // Determine file size
-    fseek(encoded_snippet, 0, SEEK_END);
-    long fileSize = ftell(encoded_snippet);
-    fseek(encoded_snippet, 0, SEEK_SET);
-    std::cout << "Voice data file size: " << fileSize << std::endl;
-    // Read the file contents into a vector
-    std::vector<char> voiceData(fileSize);
-    if (fread(voiceData.data(), 1, fileSize, encoded_snippet) != fileSize) {
-        std::cerr << "Failed to read voice data into vector" << std::endl;
-        return 1;
-    }
+    fclose(input_file);
 
     // Create a connection to the relay
     rpc::client client(RELAY_IP, 8080);
     std::this_thread::sleep_for(std::chrono::seconds(10));
     // Call the remote procedure to send the file
-    client.call("process", voiceData);
+    client.call("process", encoded_snippet);
 
+    std::cout << "Caller goodbye ..." << std::endl;
     return 0;
 }
