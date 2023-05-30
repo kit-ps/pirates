7076e61c2d6bc2bea2ebbf871c1c33b39e911b97
Christoph Coijanovic <hi@coijanovic.com>
2023-05-30 21:39:11 +0200

    added mean ratio

--- eval-logs.py
+++ eval-logs.py
@@ -4,6 +4,7 @@ import sys
 
 WARMUP_ROUNDS = 4
 
+pandas.set_option('display.max_rows', None)
 roles = ['caller', 'relay', 'worker', 'callee']
 
 # Read the csv files into separate dataframes
@@ -29,7 +30,8 @@ for experiment in list(dict.fromkeys(df['exp_id'].tolist())):
     tmp_df = df[df['exp_id'] == experiment]
     print(f"GS: {tmp_df['group_size'].iat[0]}, NU: {tmp_df['num_users'].iat[0]}, SS: {tmp_df['snippet_size'].iat[0]}")
     print(f"Mean: {tmp_df['mouth_to_ear'].iloc[WARMUP_ROUNDS:].mean()}")
-    print(f"Std. Dev.: {tmp_df['mouth_to_ear'].iloc[WARMUP_ROUNDS:].std()}\n")
+    print(f"Std. Dev.: {tmp_df['mouth_to_ear'].iloc[WARMUP_ROUNDS:].std()}")
+    print(f"Mean Ratio: {tmp_df['ratio'].iloc[WARMUP_ROUNDS:].mean()}\n")
 
 #print(df['continuous'].isin([True]))
 
@@ -38,6 +40,6 @@ for experiment in list(dict.fromkeys(df['exp_id'].tolist())):
 #df.to_csv("./logs/combined.csv")
 #print(tmp_df[['exp_id', 'group_size', 'num_users', 'snippet_size', 'mouth_to_ear', 'continuous']])
 
-tmp_df = df[df['run_id'] == '5']
-print(tmp_df[['snippet_size', 'pir_reply_time', 'ratio', 'mouth_to_ear']])
+#tmp_df = df[df['run_id'] == '5']
+#print(df[['snippet_size', 'pir_reply_time', 'ratio', 'mouth_to_ear']])
     
