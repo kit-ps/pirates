bba129f2634eab0cdb54e2520eab30ae01ad660a
Christoph Coijanovic <christoph.coijanovic@kit.edu>
2023-05-31 23:04:13 +0200

    more output for parameters helper script

--- parameters.py
+++ parameters.py
@@ -1,13 +1,16 @@
 import argparse
 import math
 
-NUM_THREAD = 64
+NUM_THREAD = 12
 
 def number_of_buckets(group_size):
     return math.ceil(1.5 * (group_size - 1))
 
 def replies_per_thread(num_users, num_bucket):
     return max(1, math.ceil((num_users * num_bucket) / NUM_THREAD))
+
+def items_per_bucket(num_users, num_bucket):
+    return math.ceil(3 * num_users / num_bucket)
 
 def main():
     # Define and parse cli flags
@@ -20,6 +23,8 @@ def main():
     args = parser.parse_args()
 
     print(f'Replies per thread: {replies_per_thread(args.users, number_of_buckets(args.groupsize))}')
+    print(f'Items per bucket: {items_per_bucket(args.users, number_of_buckets(args.groupsize))}')
+    print(f'Number of buckets: {number_of_buckets(args.groupsize)}')
 
 if __name__ == "__main__":
     main()
